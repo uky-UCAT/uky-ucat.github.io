c742a6f76e7b317e0540cc7b35cc670099cf72ca
Thakkar Research Group @ UKY <112212435+uky-UCAT@users.noreply.github.com>
2026-08-05 17:12:35 -0500

    Update sponsors.js

--- data/sponsors.js
+++ data/sponsors.js
@@ -21,4 +21,5 @@ SITE.sponsors = [
   { name:"KY Multiscale",                short:"KY Multiscale", url:"https://kymultiscale.org",  logo:"" },
   { name:"CMC Microsystems",             short:"CMC",           url:"https://www.cmc.ca",        logo:"" },
   { name:"Argonne National Laboratory",  short:"Argonne",       url:"https://www.anl.gov",       logo:"" }
+  { name:"Advanced Micro Foundry (now GlobalFoundries)",  short:"AMF/GF",       url:"https://gf.com/technologies/silicon-photonics/",       logo:"" } 
 ];
